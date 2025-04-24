416dbb942168cb4cfb4a2761b5f7cc7311cf201a
Jonathan <jonathanutsuni@gmail.com>
2025-04-24 17:22:33 -0300

    Implementação da camada de repository

--- src/main/java/br/com/fiap/locatech/locatech/LocatechApplication.java
+++ src/main/java/br/com/fiap/locatech/locatech/LocatechApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LocatechApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LocatechApplication.class, args);
+		SpringApplication.run(LocatechApplication.class, args); 
 	}
 
 }
---
+++ src/main/java/br/com/fiap/locatech/locatech/entitites/Aluguel.java
@@ -0,0 +1,38 @@
+package br.com.fiap.locatech.locatech.entitites;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Aluguel {
+
+    private Long id;
+
+    private Long pessoaId;
+
+    private Long veiculoId;
+
+    private String veiculoModelo;
+
+    private String pessoaCpf;
+
+    private String pessoaNome;
+
+    private LocalDate dataInicio;
+
+    private LocalDate dataFim;
+
+    private BigDecimal valorTotal;
+}
